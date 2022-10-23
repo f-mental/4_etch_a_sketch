00fe0a62393358c9e5322dcd4080a0384e529fe8
fmentalwinds <ffoolmental@gmail.com>
2022-10-23 10:42:49 +0800

    Build completed drawing logic
    Added logic to different pen states and added function to generate color that listens to click to random button

--- etch.js
+++ etch.js
@@ -1,4 +1,42 @@
 const largestDiv = document.querySelector('.largest-div');
+const penStateform = document.querySelector('form');
+const randomButton = document.querySelector('.random');
+const colorDiv = document.querySelector('.colorpicker');
+const colorSelect = document.querySelector('.colorselect');
+const clearButton = document.querySelector('.clear');
+
+randomButton.addEventListener('click', () => {
+    colorSelect.disabled = true;
+    randomButton.disabled = true;
+    let disableRandom = document.createElement('button');
+    disableRandom.innerText = 'Pick Color';
+    colorDiv.append(disableRandom);
+    disableRandom.addEventListener('click', () => {
+        colorSelect.disabled = false;
+        randomButton.disabled = false;
+        disableRandom.remove();
+    })
+})
+
+
+
+// check if random color
+function getColor() {
+    let color;
+    if (randomButton.disabled) {
+        let red = Math.floor((Math.random()*255));
+        let green = Math.floor((Math.random()*255));
+        let blue = Math.floor((Math.random()*255));
+        color = `rgb(${red}, ${green}, ${blue})`;
+    } else {
+        color = colorSelect.value;
+    }
+    return color;
+}
+
+
+
+
 
 // div and boxes creators
 for (let i = 0; i < 16; i++) {
@@ -8,23 +46,30 @@ for (let i = 0; i < 16; i++) {
     for (let n = 0; n < 16; n++) {
         let smallDiv = document.createElement('div');
         smallDiv.classList.add('small');
+        // clear using clearButton
+        clearButton.addEventListener('click', () =>  {
+            smallDiv.style.backgroundColor = '#ffffff';
+        })
         smallDiv.addEventListener('mouseover', () => {
-            // smallDiv.classList.add('drawn');
-            let colorSelected = document.querySelector('.colorpicker').value;
-            if (colorSelected !== '#ffffff') {
-                smallDiv.style.backgroundColor = colorSelected;
-            } else  {
-                // random color generator if a color is not selected
-                let red = Math.floor((Math.random()*255));
-                let green = Math.floor((Math.random()*255));
-                let blue = Math.floor((Math.random()*255));
-                smallDiv.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            // get current pen state
+            let currentPenState = penStateform.penstate.value;
+            let currentColor = getColor();
+            if (currentPenState === 'hover') {
+                largestDiv.setAttribute('class', 'largest-div bucket');
+                smallDiv.style.backgroundColor = currentColor;
+            } else if (currentPenState === 'precise') {
+                largestDiv.setAttribute('class', 'largest-div precise');
+                smallDiv.addEventListener('click', () => {
+                    smallDiv.style.backgroundColor = currentColor;
+                })
+            } else if (currentPenState === 'erase') {
+                largestDiv.setAttribute('class', 'largest-div erase');
+                smallDiv.addEventListener('click', () => {
+                    smallDiv.style.backgroundColor = '#ffffff';
+                })
             }
         })
         bigDiv.append(smallDiv);
     }
-
     largestDiv.append(bigDiv);
-}
-
-
+};
